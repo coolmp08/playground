5c03c52f57411c49f6d8b1ada03b0c823de6ff5a
coolmp08 <coolmp08@gmail.com>
2024-12-11 22:03:33 +0530

    changes for AtomicInteger

--- com/devSense/multithreading/atomicity/Atomic.java
+++ com/devSense/multithreading/atomicity/Atomic.java
@@ -1,55 +1,57 @@
 package com.devSense.multithreading.atomicity;
 
+import java.util.concurrent.atomic.AtomicInteger;
 
 class SharedCounter {
 
-    private int count;
+    private AtomicInteger count = new AtomicInteger(0);
 
-    public SharedCounter(int count) {
+    /*public SharedCounter(int count) {
         this.count = count;
-    }
+    }*/
 
     public SharedCounter() {
 
     }
 
     public int getCount() {
-        return count;
+        return count.get();
     }
 
-    public void setCount(int count) {
+    /*public void setCount(int count) {
         this.count = count;
-    }
+    }*/
 
     public void increment() {
-        count++;
+        count.incrementAndGet();
     }
 }
 
 public class Atomic {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         SharedCounter sharedCounter = new SharedCounter();
 
         new Thread(() -> {
             System.out.println("Thread 1 started");
-            for (int i = 0; i < 50000; i++){
+            for (int i = 0; i < 50000; i++) {
                 sharedCounter.increment();
             }
-//            Thread.sleep(1000);
             System.out.println("Thread 1 Completed");
         }).start();
+
+//        Thread.sleep(1000);
 
 
         new Thread(() -> {
             System.out.println("Thread 2 started");
-            for (int i = 0; i < 50000; i++){
+            for (int i = 0; i < 50000; i++) {
                 sharedCounter.increment();
             }
-//            Thread.sleep(1000);
             System.out.println("Thread 2 Completed");
         }).start();
+//        Thread.sleep(1000);
 
         System.out.println("Final Count is : " + sharedCounter.getCount());
     }
