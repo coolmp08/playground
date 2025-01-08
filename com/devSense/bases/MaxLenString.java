4cc840a21bcea4c37fd9d6e2089c5ac24b56e61d
coolmp08 <coolmp08@gmail.com>
2025-01-08 20:23:20 +0530

    max length string

---
+++ com/devSense/bases/MaxLenString.java
@@ -0,0 +1,22 @@
+package com.devSense.bases;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class MaxLenString {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("John");
+        names.add("Alice");
+        names.add("Bob");
+        names.add("Christopher");
+
+        // Sort the list by string length in descending order
+        names.sort(Comparator.comparingInt(String::length).reversed());
+        // Print the sorted list
+        System.out.println(names); // Output: [Christopher, Alice, John, Bob]
+        System.out.println(names.get(0)); //output: Christopher
+    }
+}
