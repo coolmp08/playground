027bf0a33da390400cb75642c0cb62cbc711734c
coolmp08 <coolmp08@gmail.com>
2025-01-08 20:26:43 +0530

    max length string change

--- com/devSense/bases/MaxLenString.java
+++ com/devSense/bases/MaxLenString.java
@@ -14,9 +14,10 @@ public class MaxLenString {
         names.add("Christopher");
 
         // Sort the list by string length in descending order
-        names.sort(Comparator.comparingInt(String::length).reversed());
+//        names.sort(Comparator.comparingInt(String::length).reversed());
+        names.sort((a,b) -> Integer.compare(a.length(),b.length()));
         // Print the sorted list
         System.out.println(names); // Output: [Christopher, Alice, John, Bob]
-        System.out.println(names.get(0)); //output: Christopher
+        System.out.println(names.get(names.size()-1)); //output: Christopher
     }
 }
