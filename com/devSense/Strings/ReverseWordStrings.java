8f2a8281123edd52cf83aa92121fd3e12a8955c4
coolmp08 <coolmp08@gmail.com>
2025-01-08 20:49:49 +0530

    Reverse String

---
+++ com/devSense/Strings/ReverseWordStrings.java
@@ -0,0 +1,43 @@
+package com.devSense.Strings;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Input: str = “i.like.this.program.very.much”
+ * Output: str = “much.very.program.this.like.i”
+ *
+ *
+ * Input: str = ”..geeks..for.geeks.”
+ * Output: str = “geeks.for.geeks”
+ */
+public class ReverseWordStrings {
+
+    public static void main (String [] args){
+
+        String str = "i.like.this.program.very.much";
+        str = "..geeks..for.geeks.";
+        String revString = ReverseWords(str);
+        System.out.println(revString);
+    }
+
+     static  String ReverseWords(String input){
+        //split with . and ignore multiple..
+
+        List<String> words = new ArrayList<>();
+        String[] parts = input.split("\\.");
+
+        for (String word : parts){
+            if(!word.isEmpty()){
+                words.add(word);
+            }
+        }
+         Collections.reverse(words);
+        return String.join(".",words);
+//        return "Hello";
+
+    }
+
+}
