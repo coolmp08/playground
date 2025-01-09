db2f458fa1e2195dbe0ebd6e73d5774b18b06bb4
coolmp08 <coolmp08@gmail.com>
2025-01-09 19:17:26 +0530

    Rotate Array elements CounterClockwise

--- com/devSense/arrays/RotateArrayCounterClockwisev1.java
+++ com/devSense/arrays/RotateArrayCounterClockwisev1.java
@@ -1,8 +1,6 @@
 package com.devSense.arrays;
 
 
-import java.util.Arrays;
-
 /**
  * Input: arr[] = {1, 2, 3, 4, 5, 6}, d = 2
  * Output: {3, 4, 5, 6, 1, 2}
@@ -23,32 +21,37 @@ import java.util.Arrays;
  * (the first element becomes the last).
  * Perform this operation d times to rotate the elements to the left by d positions.
  */
-public class RotateArrayCounterClockwise {
+
+public class RotateArrayCounterClockwisev1 {
 
     public static void main(String[] args) {
+        Integer[] arr = {1, 2, 3, 4, 5, 6};
+        int d = 2;
 
-//        Integer arr[] = {1, 2, 3, 4, 5, 6}, d = 4;
-        Integer arr[] = {1, 2, 3}, d = 4;
-        Integer ja[] = rotateArrayCounterClockwise(arr, d);
+        rotateArrayCounterClockwise(arr, d);
 
-        System.out.println(Arrays.asList(ja));
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+    }
+
+    static void rotateArrayCounterClockwise(Integer[] arr, int d) {
+        int n = arr.length;
+
+        reverse(arr, 0, d - 1);
+
+        reverse(arr, d, n - 1);
+
+        reverse(arr, 0, n - 1);
 
     }
 
-    // Function to left rotate array by d positions
-    static Integer[] rotateArrayCounterClockwise(Integer[] arr, Integer d) {
-
-        int n = arr.length;
-
-        // Repeat the rotation d times
-        for (int i = 0; i < d; i++) {
-            int first = arr[0];
-            for (int j = 0; j < n - 1; j++) {
-                arr[j] = arr[j + 1];
-            }
-            arr[n - 1] = first;
+    static void reverse(Integer[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
         }
-
-        return arr;
     }
 }
